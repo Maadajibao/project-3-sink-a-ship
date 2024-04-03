4874cc204fd1d564d7adf992caada4b0f8ff6c70
Maada Jibao <maada.jibao01@gmail.com>
2024-04-03 14:18:28 +0000

    Create welcome function that informs player for the game rules and promp for player name

--- run.py
+++ run.py
@@ -2,6 +2,24 @@ from random import randint
 
 
 
+def welcome():
+    """
+    Welcome string that prompt for player name
+    """
+
+    print("Welcome to Sink A Ship, a battleship game\n")
+    print("You as our general will command a feelt of 4 ship which will be randomly generated on the playboard\n")
+    print("-" *35)
+    print("\nMission:""You task is to Sink All Enemy Ships\n")
+    print("-" *35)
+    print(f"\nBoard Size: | Number of Ships")
+    print("Top left corner is row: 0, col: 0\n")
+    print("-" *35)
 
 
 def game_run():
+
+    welcome()
+
+
+game_run()
