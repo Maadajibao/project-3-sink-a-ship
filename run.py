6520b4bc97bf9bc0a40773f4cac4e8931c35477b
Maada Jibao <maada.jibao01@gmail.com>
2024-04-04 04:31:37 +0000

    Tried to remove whitspace

--- run.py
+++ run.py
@@ -2,7 +2,9 @@ from random import randint
 
 scores = {"computer": 0, "player": 0}
 
+
 class Board:
+
     """
     Main board class. Sets Board size, the number of ships,
     the player's name and the board type(player or computer)
@@ -10,7 +12,7 @@ class Board:
     """
 
     def __init__(self, size, num_ships, name, board_type):
-        self.size = size 
+        self.size = size
         self.board = [["." for x in range(size)] for y in range(size)]
         self.num_ships = num_ships
         self.name = name
@@ -29,13 +31,12 @@ class Board:
 
         for row in self.board:
             print(" ".join(row))
-        
+    
     
     def add_ships(self, x, y):
-        # in case we extend code so user can set amout of ships    
-
-
-        self.ships.append((x,y))
+        
+        # in case we extend code so user can set amout of ships
+        self.ships.append((x, y))
         if self.type == "player":
             self.board[x][y] = "@"
 
@@ -45,9 +46,9 @@ class Board:
         Accepts the coordinates of a guess and updates the board accordingly.
         Returns a message indicating whether the guess was hit or miss
         """
-
-        self.guesses.append((x,y))
         
+        
+        self.guesses.append((x,y))
 
         if (x, y) in self.ships:
             self.board[x][y] = "*"
@@ -58,11 +59,11 @@ class Board:
             return f"Slaash! It Misses {self.name}"
 
 
-    
 
 def random_point(size):
     """
-    Generate a random number between 0 and the length of the board(size) minus one.
+    Generate a random number between 0
+    and the length of the board(size) minus one.
     We minus 1 for the size of the board we set.
     """
     return randint(0, size - 1)
@@ -70,7 +71,8 @@ def random_point(size):
 
 def populate_board(board):
     """
-    Will populate x with random_point and the same with y with ships from add_ship function.
+    Will populate x with random_point
+    and the same with y with ships from add_ship function.
     """
 
     for _ in range(board.num_ships):
@@ -97,8 +99,8 @@ def valid_coordinates(x, y):
         y = int(y)
 
         if x < 0 or x > 4 or y < 0 or y > 4:
-            raise ValueError("Your shot is out of bounds! Choose a number within the board's range")
-    
+            raise ValueError("Your shot is out of bounds!", 
+            "Choose a number within the board's range")
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.")
         print("Please enter an number between 1 and 5")
@@ -109,7 +111,8 @@ def valid_coordinates(x, y):
 
 def make_guess(board):
     """
-    Prompt the user to enter coordinates for a guess and checks if they are valid.
+    Prompt the user to enter coordinates
+    for a guess and checks if they are valid.
     If valid, returns the coordinates as tuple (x, y), otherwise returns None.
     """
 
@@ -118,9 +121,9 @@ def make_guess(board):
         y = input("Enter column (0-4): \n")
 
         if valid_coordinates(x, y):
-            x = int(x) 
+            x = int(x)
             y = int(y)
-            if (x,y) not in board.guesses:  # Checks if the have not been guessed before
+            if (x, y) not in board.guesses:  # Checks if the have not been guessed before
                 return x, y
             else:
                 print("\nYou'have already guessed these coordinates. Please try again.\n")
@@ -137,16 +140,15 @@ def welcome():
 
     print("Welcome to Sink A Ship, a battleship game\n")
     print("You as our general will command a feelt of 4 ship which will be randomly generated on the playboard\n")
-    print("-" *35)
+    print("-" * 35)
     print("\nMission:""You task is to Sink All Enemy Ships\n")
-    print("-" *35)
+    print("-" * 35)
     print(f"\nBoard Size:  | Number of Ships: ")
     print("Top left corner is row: 0, col: 0\n")
-    print("-" *35)
+    print("-" * 35)
     print("Whats your name General?:\n")
     
-
-
+    
 def run_game(computer_board, player_board):
     """
     Runs the game loop until one of the players wins
@@ -162,18 +164,16 @@ def run_game(computer_board, player_board):
             x, y = player_guess
             player_result = computer_board.guess(x, y)
             player_board.guesses.append(player_guess)
-
-            if  "Boom" in player_result:  # Check if the player hit a shit so that i can increment it
+            
+            
+            # Check if the player hit a shit so that i can increment it
+            
+            if  "Boom" in player_result:  
                 scores["player"] += 1
 
             if not computer_board.ships:
                 print("Congratulations! You have sunk all the enemy ships!")
                 break
-        
-    
-
-
-        
         # Computer's turn
         print("\nComputer's turn:")
         computer_guess = (random_point(len(player_board.board)), random_point(len(player_board.board)))
@@ -187,7 +187,6 @@ def run_game(computer_board, player_board):
         if not player_board.ships:
             print("The computer has sunk all you ships. You lose!")
             break
-        
 
         # Prints both player's and computer's boards after each turn
 
@@ -270,6 +269,3 @@ def start_game():
 
 
 start_game()
-
-
-
